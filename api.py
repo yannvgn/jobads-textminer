559e9b65155ecb20c002f845479dc4b777236b2f
amine <mejrimedamine92@gmail.com>
2017-02-06 16:09:04 +0100

    configure api

---
+++ api.py
@@ -0,0 +1,17 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+import jobads.fetch.ads 
+
+app = Flask(__name__)
+
+
+
+ 
+
+@app.route('/api/ads/search/<q>', methods=['GET'])
+def get_ads(q):
+    ads=jobads.fetch.ads.getAdsBySimpleQuery(q)
+    return jsonify(ads)
+
+if __name__ == '__main__':
+    app.run(debug=True)
