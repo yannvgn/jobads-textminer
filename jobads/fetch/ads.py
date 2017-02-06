55e784b76c7fd3c289d2dd01d8d405bdf51ca54e
Yann <yann.vaginay@gmail.com>
2017-02-06 15:28:26 +0100

    fixed key error

--- jobads/fetch/ads.py
+++ jobads/fetch/ads.py
@@ -7,5 +7,5 @@ def getAdsBySimpleQuery(q):
     esResult = es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], q=q)
     results = []
     for result in esResult['hits']['hits']:
-        results.append(result['source'])
+        results.append(result['_source'])
     return results
