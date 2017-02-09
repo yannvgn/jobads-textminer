2c0aec2a8a17bf7074afa220b51e6e462156fdbd
Yann <yann.vaginay@gmail.com>
2017-02-09 15:50:27 +0100

    simple ad fetch: query string replaced by multi_match

--- jobads/fetch/ads.py
+++ jobads/fetch/ads.py
@@ -4,7 +4,17 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch(hosts=config['elasticsearch']['hosts'], verify_certs=True)
 
 def getAdsBySimpleQuery(q):
-    esResult = es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], q=q)
+    if type(q) != str:
+        return []
+    
+    esResult = es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], body={
+        'query': {
+            'multi_match' : {
+                'query':    q,
+                'fields': [ 'title_fr', 'description_fr', 'company', 'location' ]
+            }
+        }
+    })
     results = []
     for result in esResult['hits']['hits']:
         results.append(result['_source'])
