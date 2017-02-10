d1d27f47c4419f919aaacdd6352c09c133b9772f
Yann <yann.vaginay@gmail.com>
2017-02-10 10:41:25 +0100

    fetch ads api updated

--- jobads/fetch/ads.py
+++ jobads/fetch/ads.py
@@ -3,19 +3,31 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(hosts=config['elasticsearch']['hosts'], verify_certs=True)
 
+def _formatResult(hit):
+    result = hit['_source']
+    result['_id'] = hit['_id']
+    result['_score'] = hit['_score']
+    return result
+
+def _formatQueryResponse(esResult):
+    return {
+        'results': [_formatResult(hit) for hit in esResult['hits']['hits']],
+        'total': esResult['hits']['total'],
+        'max_score': esResult['hits']['max_score']
+    }
+
+def _queryElastic(**args):
+    return es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], **args)
+
 def getAdsBySimpleQuery(q):
     if type(q) != str:
-        return []
+        return {}
     
-    esResult = es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], body={
+    return _formatQueryResponse(_queryElastic(body={
         'query': {
             'multi_match' : {
                 'query':    q,
                 'fields': [ 'title_fr', 'description_fr', 'company', 'location' ]
             }
         }
-    })
-    results = []
-    for result in esResult['hits']['hits']:
-        results.append(result['_source'])
-    return results
+    }))
