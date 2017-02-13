c74640ff4e61597660e625227e0ec07af41952ed
Yann <yann.vaginay@gmail.com>
2017-02-13 20:43:35 +0100

    refactored ads api

--- api.py
+++ api.py
@@ -4,57 +4,109 @@ import jobads.fetch.ads
 
 app = Flask(__name__)
 
+# Routing
+
+# Deprecated
 @app.route('/api/ads/search/<q>', methods=['GET'])
-def get_ads(q):
-    limit=request.args.get("limit",10)
-    offset=request.args.get("offset",0)
+def get_ads_legacy(q):
+    limit, offset = _getLimitOffset(request)
+    filters = _getFilters(request)
+    ads=jobads.fetch.ads.getAdsBySimpleQuery(q=q, limit=limit, offset=offset, filters=filters)
+    return jsonify(ads)
+
+@app.route('/api/ads/search', methods=['POST', 'GET'])
+def get_ads():
+    limit, offset = _getLimitOffset(request)
+    filters = _getFilters(request)
+    q = _getRequiredParam(request, 'q')
+    ads=jobads.fetch.ads.getAdsBySimpleQuery(q=q, limit=limit, offset=offset, filters=filters)
+    return jsonify(ads)
+
+@app.route('/api/ads/get/<ids>', methods=['GET'])
+def get_ads_by_ids(ids):
+    ads = jobads.fetch.ads.getAdsByIds(ids.split(','))
+    return jsonify(ads)
+
+# Deprecated
+@app.route('/api/ads/coords/search/<q>', methods=['GET'])
+def get_ads_coords_legacy(q):
+    coords=jobads.fetch.ads.getAdsCoordsBySimpleQuery(q, filters=_getFilters(request))
+    return jsonify(coords)
+
+@app.route('/api/ads/coords/search', methods=['POST', 'GET'])
+def get_ads_coords():
+    q = _getRequiredParam(request, 'q')
+    coords=jobads.fetch.ads.getAdsCoordsBySimpleQuery(q, filters=_getFilters(request))
+    return jsonify(coords)
+
+@app.route('/api/ads/get_basic_info', methods=['POST', 'GET'])
+def get_ads_basic_info():
+    ids = _getRequiredParam(request, 'ids')
+    if type(ids) != list:
+        ids = str(ids).split(',')
     
-    jobtype = request.args.get('jobtype', None)
+    basic_info=jobads.fetch.ads.getAdsBasicInfoByIds(ids)
+    return jsonify(basic_info)
 
-    geofilter = None    
-    lat = request.args.get('lat', None)
-    lon = request.args.get('lon', None)
-    dist = request.args.get('dist', None)
 
-    try:
-        limit=int(limit)
-        offset=int(offset)
-    except ValueError:
+# Internal functions
+
+def _getParam(req, key, default=None):
+    return req.get_json().get(key, default) if req.is_json else req.args.get(key, default)
+
+def _getRequiredParam(req, key, errorCode=400):
+    p = _getParam(req, key)
+    if p == None:
         abort(400)
-    if (limit < 0 or offset < 0):
-        abort(400)
+    else:
+        return p
 
+def _getLimitOffset(req):
+    limit = _getParam(req, 'limit')
+    offset = _getParam(req, 'offset')
+    if limit != None:
+        try:
+            limit = int(limit)
+            if limit < 0:
+                abort(400)
+        except ValueError:
+            abort(400)
+    if offset != None:
+        try:
+            offset = int(offset)
+            if offset < 0:
+                abort(400)
+        except ValueError:
+            abort(400)
+    return limit, offset
+    
+def _getFilters(req):
+    filters = {}
+    filters.update(_getJobtypeFilter(req))
+    filters.update(_getGeoFilter(req))
+    return filters
+
+def _getJobtypeFilter(req):
+    jobtype = _getParam(req, 'jobtype')
+    if jobtype != None:
+        return {'jobtype': jobtype}
+    else:
+        return {}
+
+def _getGeoFilter(req):
+    lat = _getParam(req, 'lat')
+    lon = _getParam(req, 'lon')
+    dist = _getParam(req, 'dist')
     if (lat != None and lon != None and dist != None):
         try:
             lat = float(lat)
             lon = float(lon)
-            geofilter = {'lat': lat, 'lon': lon, 'dist': dist}
+            return {'geodistance': {'lat': lat, 'lon': lon, 'dist': dist}}
         except ValueError:
             abort(400)
-
-    ads=jobads.fetch.ads.getAdsBySimpleQuery(q=q, limit=limit, offset=offset, jobtype=jobtype, geofilter=geofilter)
-    return jsonify(ads)
-
-@app.route('/api/ads/search', methods=['POST'])
-def get_ads_post():
-    data = request.get_json()
-    limit=request.args.get("limit")
-    offset=request.args.get("offset")
-    if data and 'q' in data:
-        ads=jobads.fetch.ads.getAdsBySimpleQuery(q=str(data['q']), limit=limit, offset=offset)
-        return jsonify(ads)
     else:
-        abort(400)
+        return {}
 
-@app.route('/api/ads/get/<q>', methods=['GET'])
-def get_ads_by_ID(q):
-    ads = jobads.fetch.ads.getAdsByIds(q.split(','))
-    return jsonify(ads)
-
-@app.route('/api/ads/coords/search/<q>', methods=['GET'])
-def get_ads_coords(q):
-    coords=jobads.fetch.ads.getAdsCoordsBySimpleQuery(q)
-    return jsonify(coords)
 
 
 if __name__ == '__main__':
--- jobads/fetch/ads.py
+++ jobads/fetch/ads.py
@@ -3,11 +3,71 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(hosts=config['elasticsearch']['hosts'], verify_certs=True)
 
-def _formatResult(hit):
-    result = hit['_source']
-    result['_id'] = hit['_id']
-    if '_score' in hit:
-        result['_score'] = hit['_score']
+def getAdsBySimpleQuery(q,limit=None,offset=None,filters={}):
+    if limit == None:
+        limit = 10
+    if offset == None:
+        offset = 0
+
+    return _formatQueryResponse(_queryElastic(body={
+        'from': offset, 'size': limit,
+        'query': {
+            'bool': {
+                'must': {
+                    'multi_match': {
+                        'query': str(q),
+                        'fields': ['title_fr', 'description_fr', 'company', 'location']
+                    }
+                },
+                'filter': _makeFilterBody(filters)
+            }
+        }
+    }))
+
+def getAdsByIds(ids):
+    return {
+        'results': [_formatResult(doc) for doc in _mgetQuery(body={'ids' : ids})['docs']]
+    }
+
+def getAdsBasicInfoByIds(ids):
+    return {
+        'results': [_formatResult(doc) for doc in _mgetQuery(body={'ids' : ids}, _source=['company', 'title_fr', 'jobtype'])['docs']]
+    }
+
+def getAdsCoordsBySimpleQuery(q, filters={}):
+    nb_max_results = 1000
+    filterBody = _makeFilterBody(filters)
+    filterBody.append({'exists': {'field': 'geolocation'}})
+
+    esResult = _queryElastic(_source=['geolocation'], body={
+        'query': { 
+            'bool': {
+                'must': {
+                    'multi_match': {
+                        'query': str(q),
+                        'fields': [ 'title_fr', 'description_fr', 'company', 'location']
+                    }
+                },
+                'filter': filterBody
+            }
+        },
+        'size': nb_max_results
+    })
+
+    return _formatQueryResponse(esResult)
+
+
+
+# Internal functions
+
+def _formatResult(r):
+    if r.get('found') == False:
+        return None
+
+    result = r['_source']
+    result['_id'] = r['_id']
+    if '_score' in r:
+        result['_score'] = r['_score']
     return result
 
 def _formatQueryResponse(esResult):
@@ -20,69 +80,21 @@ def _formatQueryResponse(esResult):
 def _queryElastic(**args):
     return es.search(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'], **args)
 
-def getAdsBySimpleQuery(q,limit=10,offset=0,jobtype=None,geofilter=None):
-    q = str(q)
-    
-    filters = []
-
-    if jobtype:
-        filters.append({'term': {'jobtype': jobtype}})
-    if geofilter:
-        filters.append({
-            'geo_distance' : {
-                'distance' : str(geofilter['dist']),
-                'geolocation' : {
-                    'lat' : float(geofilter['lat']),
-                    'lon' : float(geofilter['lon'])
-                }
-            }
-        })
-
-    return _formatQueryResponse(_queryElastic(body={
-        'from' : offset, 'size' : limit,
-        'query': {
-            'bool': {
-                'must': {
-                    'multi_match' : {
-                        'query':    q,
-                        'fields': [ 'title_fr', 'description_fr', 'company', 'location' , 'id' ]
-                    }
-                },
-                'filter': filters
-            }
-        }
-    }))
-
 def _mgetQuery(**args):
     return es.mget(index=config['elasticsearch']['job_ads_index'], doc_type=config['elasticsearch']['ad_doc_type'],  **args)
 
-
-def getAdsByIds(ids):
-    return {
-        'results': [_formatResult(doc) for doc in _mgetQuery(body={'ids' : ids})['docs']]
-    }
-
-def getAdsCoordsBySimpleQuery(q):
-    nb_max_results = 1000
-
-    q = str(q)
-
-    esResult = _queryElastic(_source=['geolocation'], body={
-        'query': { 
-            'bool': {
-                'must': {
-                    'multi_match' : {
-                        'query':    q,
-                        'fields': [ 'title_fr', 'description_fr', 'company', 'location']
-                    }
-                },
-                'filter': {
-                    'exists': {'field': 'geolocation'}
+def _makeFilterBody(filters):
+    filterBody = []
+    if 'jobtype' in filters:
+        filterBody.append({'term': {'jobtype': filters['jobtype']}})
+    if 'geodistance' in filters:
+        filterBody.append({
+            'geo_distance' : {
+                'distance' : str(filters['geodistance']['dist']),
+                'geolocation' : {
+                    'lat' : float(filters['geodistance']['lat']),
+                    'lon' : float(filters['geodistance']['lon'])
                 }
             }
-        },
-        'size': nb_max_results
-    })
-
-    return _formatQueryResponse(esResult)
-
+        })
+    return filterBody
