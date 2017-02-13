f7d4db5dfef6760c04281b4e2ec2baf31748f1c3
amine <mejrimedamine92@gmail.com>
2017-02-13 11:28:48 +0100

    offset and limit settings

--- api.py
+++ api.py
@@ -6,14 +6,29 @@ app = Flask(__name__)
 
 @app.route('/api/ads/search/<q>', methods=['GET'])
 def get_ads(q):
-    ads=jobads.fetch.ads.getAdsBySimpleQuery(q)
-    return jsonify(ads)
+    limit=request.args.get("limit",10)
+    offset=request.args.get("offset",0)
+    #print(offset)
+    
+    try:
+        limit=int(limit)
+        offset=int(offset)
+    except ValueError:
+        print("error in casting")
+
+    if (int(limit)<0 or int(offset)<0):
+        abort(400)
+    else:
+        ads=jobads.fetch.ads.getAdsBySimpleQuery(q, limit, offset)
+        return jsonify(ads)
 
 @app.route('/api/ads/search', methods=['POST'])
 def get_ads_post():
     data = request.get_json()
+    limit=request.args.get("limit")
+    offset=request.args.get("offset")
     if data and 'q' in data:
-        ads=jobads.fetch.ads.getAdsBySimpleQuery(str(data['q']))
+        ads=jobads.fetch.ads.getAdsBySimpleQuery(str(data['q']), limit, offset)
         return jsonify(ads)
     else:
         abort(400)
